0969dd626f6b22b06df2bf24fba1430acef89de2
sagar0211 <sagarparihar002@gmail.com>
2020-09-28 18:10:02 +0530

    eslint & functional component changed

--- App.js
+++ App.js
@@ -1,48 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View ,Button } from 'react-native';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-use-before-define */
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, View, Button,
+} from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    count : 0
-  }
-  increment =() => {
-    this.setState({count: this.state.count + 1})
-  }
-  decrement =() => {
-    this.setState({count: this.state.count - 1})
-  }
-render() {
+export default function App() {
+  const [count, SetCount] = useState(0);
+  const increment = () => {
+    SetCount(count + 1);
+  };
+  const decrement = () => {
+    SetCount(count - 1);
+  };
   return (
-    <View >
-      <Text style={styles.header}>
-        This is header
-      </Text>
-      <Button 
-      title="Increment button"
-      onPress={this.increment}
-      />
-      <Text style={styles.show} >{this.state.count}</Text>
-      <Button 
-      title="Decrement button "
-      onPress={this.decrement} 
-      /> 
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>
+          Counter
+        </Text>
+        <Button
+          style={styles.button}
+          title="Increment button"
+          onPress={increment}
+        />
+        <Text style={styles.show}>{count}</Text>
+        <Button
+          style={styles.button}
+          title="Decrement button "
+          onPress={decrement}
+        />
+      </View>
     </View>
   );
 }
-}
-
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   header: {
-    width:'100vw',
-    height:'10vh',
-    textAlign:'center',
-    backgroundColor: 'black',
-    color:'white',
+    width: '100vw',
+    height: '10vh',
+    padding: '5px',
+    textAlign: 'center',
+    color: 'black',
     fontSize: 30,
-  }
+  },
+  show: {
+    textAlign: 'center',
+  },
 
 });
-
-
- 
